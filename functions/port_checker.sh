cebee49b6f8aea79319832e0b4b05568d6c3c2d0
Hideyuki-T <hide27yuki@gmail.com>
2025-04-15 10:51:06 +0900

    [add]port_checker.sh

---
+++ functions/port_checker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+check_port_availability() {
+  local port ="$1"
+  while lsof -i TCP:"${port}" -sTCP:LISTEN >/dev/null 2>&1; do
+    log_warn "ポート　${port} は使用中なので +1して設定します。"
+    port=$((port + 1))
+    done
+    echo "$port"
+}
