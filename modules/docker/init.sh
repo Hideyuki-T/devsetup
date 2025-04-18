16a4d0708face1d4fb8c7399e5ebfe55833e5dd5
Hideyuki-T <hide27yuki@gmail.com>
2025-04-19 08:52:36 +0900

    [fix]

--- modules/docker/init.sh
+++ modules/docker/init.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+# Docker モジュール：初期化フェーズ
+
 log_info "modules/docker/init.sh：テンプレートをコピー中…"
-cp -R templates/php-nginx-mysql/. .
+
+# 1) docker ディレクトリをコピー
+cp -R templates/php-nginx-mysql/docker ./docker
+
+# 2) docker-compose.yml テンプレートをリネームしつつコピー
+cp templates/php-nginx-mysql/docker-compose.yml.template ./docker-compose.yml
+
 log_info "modules/docker/init.sh：コピー完了"
