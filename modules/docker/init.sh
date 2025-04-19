ac102ce2bc79dd87b9ff07327f0e058811159841
Hideyuki-T <hide27yuki@gmail.com>
2025-04-19 09:00:04 +0900

    [fix]

--- modules/docker/init.sh
+++ modules/docker/init.sh
@@ -3,10 +3,14 @@
 
 log_info "modules/docker/init.sh：テンプレートをコピー中…"
 
-# 1) docker ディレクトリをコピー
-cp -R templates/php-nginx-mysql/docker ./docker
+# 1) 作業先の docker ディレクトリを確保
+mkdir -p docker
 
-# 2) docker-compose.yml テンプレートをリネームしつつコピー
-cp templates/php-nginx-mysql/docker-compose.yml.template ./docker-compose.yml
+# 2) nginx と php の設定をそれぞれコピー
+cp -R templates/php-nginx-mysql/docker/nginx    docker/
+cp -R templates/php-nginx-mysql/docker/php      docker/
+
+# 3) docker-compose ファイルを正しい名前で出力
+cp templates/php-nginx-mysql/docker-compose.yml.template docker-compose.yml
 
 log_info "modules/docker/init.sh：コピー完了"
