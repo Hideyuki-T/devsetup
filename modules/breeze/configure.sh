90e4684e8bd00d58c14459e4a775b19666d116da
Hideyuki-T <hide27yuki@gmail.com>
2025-04-20 08:38:57 +0900

    [fix]modules/breeze/configure.sh

--- modules/breeze/configure.sh
+++ modules/breeze/configure.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-#Breeze モジュール設定フェーズ
+# modules/breeze/configure.sh：設定フェーズ
 
-log_info "[INFO]: modules/breeze/configure.sh:Breeze のインストールコマンドを実行中だよ。。。"
-
-cd src
-
-#Breeze の認証スキャフォールド実行
-docker compose exec app php artisan breeze:install --ansi
-
-log_info "[INFO]: modules/breeze/configure.sh: npm・vite の依存もまとめてセットアップするよ！"
-
-docker compose exec app npm install --silent
-
-log_info "[INFO]: modules/breeze/configure.sh: 設定フェーズ終了！"
+log_info "modules/breeze/configure.sh：特に設定はありません。"
