3c13abfb57db153a55115474dbbcc99b5995f7ed
Hideyuki-T <hide27yuki@gmail.com>
2025-04-21 08:30:00 +0900

    [update]modules/laravel/init.sh

--- modules/laravel/init.sh
+++ modules/laravel/init.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # modules/laravel/init.sh
 
-log_info "[INFO]: modules/laravel/init.sh：コンテナ内で Laravel プロジェクトを作成中…"
+# 1) プロジェクト名とlaravelバージョンを読み込む
+read -rp "Project name: " PROJECT
+read -rp "Laravel version (default: 12.*):" VERSION
+VERSION=${VERSION:-12.*}
 
+log_info "[INFO]: modules/laravel/init.sh: コンテナ内で laravelプロジェクトを作成中"
+
+# 2) コンテナ内で create-project を実行
 docker compose exec app \
-  composer create-project laravel/laravel /var/www/html --prefer-dist
-
-log_info "[SUCCESS]: modules/laravel/init.sh：完了"
+  composer create-project laravel/laravel "/var/html/www/${PROJECT}" "${VERSION}" --prefer-dist
+log_info "[SUCCESS]: modules/laravel/init.sh:完了 (./${PROJECT}に Laravel ${VERSION} を生成したよ。)"
