6a551f5fef789120e935b08b97b5467deac79cf4
Hideyuki-T <hide27yuki@gmail.com>
2025-05-08 11:12:45 +0900

    [update]menu/init.sh

--- modules/menu/init.sh
+++ modules/menu/init.sh
@@ -26,11 +26,11 @@ log_info "modules/menu/init.sh：プロジェクトディレクトリを作成�
 # 4) 構成メニュー表示
 cat << 'EOF'
 メニュー：
- [1] docker(PHP + nginx + MySQL)
- [2] docker(PHP + nginx + MySQL) + Laravel
- [3] docker(PHP + nginx + MySQL) + Laravel + Breeze
- [4] docker(PHP + nginx + MySQL) + Laravel + Breeze + OAuth
- [5] docker(PHP + nginx + MySQL) + Symfony
+ [1] docker(PHP + nginx + MySQL) : 8081
+ [2] docker(PHP + nginx + MySQL) + Laravel : 8082
+ [3] docker(PHP + nginx + MySQL) + Laravel + Breeze : 8083
+ [4] docker(PHP + nginx + MySQL) + Laravel + Breeze + OAuth :8080
+ [5] docker(PHP + nginx + MySQL) + Symfony : 8085
 EOF
 read -rp "番号を選択してね。: " SELECTED
 
